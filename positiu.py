6179e5370144ccbbcf1524a42cf524459465172c
AlexMaciasAlcantud <44800183+AlexMaciasAlcantud@users.noreply.github.com>
2018-11-08 11:23:26 +0100

    Create positiu.py

---
+++ positiu.py
@@ -0,0 +1,10 @@
+#Programa que llegeixi un nombre i digui si es positiu o no
+
+n=int(input("ingresa un numero "))
+if(n==0):
+ print("el numero " + " es positiu ")
+else:
+     if(n>0):
+      print("el numero " + " es positivo")
+     else:
+      print("el numero " + " es no positiu")
