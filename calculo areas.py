030476218f2652f6b4cea7f6acff002a83e9c7ff
AlexMaciasAlcantud <44800183+AlexMaciasAlcantud@users.noreply.github.com>
2018-11-13 13:39:24 +0100

    Update calculo areas.py

--- calculo areas.py
+++ calculo areas.py
@@ -1,7 +1,7 @@
 figura = input("Que vols calcular un T o un C?")
 if (figura == "C"):
     radi= float(input("Quin es el radi del cercle:"))
-    print("El teva area es de" ,3,14*radi^2,)
+    print("La teva area es de" ,3,14*radi^2,)
 else:
     if(figura == "T"):
         base = float(input("Quina es la base:"))
